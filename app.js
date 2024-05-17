c1bc069e958db0d087445a8a838a7809552659e8
PauloPivatto38 <paulopivatto@hotmail.com>
2024-05-17 19:27:57 -0300

    create a loop to show quantity

--- app.js
+++ app.js
@@ -2,4 +2,18 @@ function draw() {
   let quantity = parseInt(document.getElementById("quantity").value);
   let fromNumber = parseInt(document.getElementById("fromNumber").value);
   let toNumber = parseInt(document.getElementById("toNumber").value);
+
+  let drawn = [];
+  let number;
+
+  for (let i = 0; i < quantity; i++) {
+    number = getRandomNumber(fromNumber, toNumber);
+    drawn.push(number);
+  }
+
+  alert(drawn);
+}
+
+function getRandomNumber(min, max) {
+  return parseInt(Math.random() * (max - min + 1) + min);
 }
