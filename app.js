376d52f003f1e7ae0ef7d195eeda35a59b83b403
PauloPivatto38 <paulopivatto@hotmail.com>
2024-05-17 19:01:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function draw() {
+  let quantity = parseInt(document.getElementById("quantity").value);
+  let fromNumber = parseInt(document.getElementById("fromNumber").value);
+  let toNumber = parseInt(document.getElementById("toNumber").value);
+}
