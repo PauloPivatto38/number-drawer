d0552b66794ec5f16bc24e0e27127a5557a804e0
PauloPivatto38 <paulopivatto@hotmail.com>
2024-05-21 20:44:51 -0300

    create functions to restart the game

--- app.js
+++ app.js
@@ -18,8 +18,40 @@ function draw() {
 
   let result = document.getElementById("result");
   result.innerHTML = `<label class="texto__paragrafo">Drawn numbers: ${drawn}</label>`;
+
+  buttonStatusChange();
 }
 
 function getRandomNumber(min, max) {
   return parseInt(Math.random() * (max - min + 1) + min);
+}
+
+function buttonStatusChange() {
+  let button = document.getElementById("btn-restart");
+
+  if (button.classList.contains("container__botao-desabilitado")) {
+    button.classList.remove("container__botao-desabilitado");
+    button.classList.add("container__botao");
+  } else {
+    // button.classList.remove("container__botao");
+    // button.classList.add("container__botao-desabilitado");
+  }
+}
+
+function restart() {
+  document.getElementById("quantity").value = "";
+  document.getElementById("fromNumber").value = "";
+  document.getElementById("toNumber").value = "";
+  document.getElementById("result").innerHTML =
+    '<label class="texto__paragrafo">Numbers drawn: none until now</label>';
+  buttonStatusReturn();
+}
+
+function buttonStatusReturn() {
+  let buttonReturn = document.getElementById("btn-restart");
+
+  if (buttonReturn.classList.contains("container__botao")) {
+    buttonReturn.classList.remove("container__botao");
+    buttonReturn.classList.add("container__botao-desabilitado");
+  }
 }
